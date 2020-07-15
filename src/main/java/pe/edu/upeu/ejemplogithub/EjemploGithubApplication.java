ced1f4e6c4489dba2dcd4e8d76b27ea8790177f7
dreyna <reyna_b@upeu.edu.pe>
2020-07-14 22:36:34 -0500

    V1

---
+++ src/main/java/pe/edu/upeu/ejemplogithub/EjemploGithubApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.ejemplogithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upeu/ejemplogithub/EjemploGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.ejemplogithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
